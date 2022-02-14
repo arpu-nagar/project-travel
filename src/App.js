a81afaf605c27c458289f18070252982ea856752
Arpan Nagar <arpunagar@gmail.com>
2022-02-14 16:50:11 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Unsplash, { toJson } from 'unsplash-js';
+import SearchPhotos from './searchPhotos';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1 className="heading">TRAVELAND 📷</h1>
+			<h3 className="subt">
+				Wander around dream cities while listen to their favourite beats!{' '}
+			</h3>
+			<SearchPhotos />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/searchPhotos.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import Unsplash, { toJson } from 'unsplash-js';
+import YouTube from 'react-youtube';
+import ReactPlayer from 'react-player/lazy';
+import axios from 'axios';
+
+const unsplash = new Unsplash({
+	accessKey: 'dcO2Wt8pFYFrf9Cr3Q2Yj2IPdnSs2gwtjz6N1ONLnGc',
+});
+
+const opts = {
+	height: '200px',
+	width: '200px',
+	playerVars: {
+		autoPlay: 1,
+	},
+};
+function _onReady(event) {
+	// access to player in all event handlers via event.target
+	event.target.pauseVideo();
+}
+export default function SearchPhotos() {
+	const [query, setQuery] = useState('');
+	const [pics, setPics] = useState([]);
+	const [yt, setYT] = useState({
+		load: false,
+		id: '',
+		url: '',
+		play: true,
+	});
+	const searchPhotos = async (e) => {
+		e.preventDefault();
+		setYT({
+			load: false,
+			id: 'MhlecL-gx4s',
+			url: '',
+			play: true,
+		});
+		unsplash.search
+			.photos(query)
+			.then(toJson)
+			.then(async (json) => {
+				// console.log(json);
+				setPics(json.results);
+				const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${query}%20song&type=video&key=AIzaSyAo3MaZpnMQ8lIio4RGglWGg3uH1Kk1IVw`;
+				const load = await axios.get(url);
+				console.log(load.data.items[0].id.videoId);
+				setYT({
+					load: true,
+					id: load.data.items[0].id.videoId,
+					url:
+						'https://www.youtube.com/watch?v=' + load.data.items[0].id.videoId,
+				});
+			});
+	};
+
+	return (
+		<>
+			<form className="form" onSubmit={searchPhotos}>
+				{' '}
+				<label className="label" htmlFor="query">
+					{' '}
+				</label>
+				<input
+					type="text"
+					name="query"
+					className="input"
+					placeholder={`Search a city you wanna visit. :)`}
+					value={query}
+					onChange={(e) => setQuery(e.target.value)}
+				/>
+				<button type="submit" className="button">
+					Search
+				</button>
+				{/* {yt.load ? ( */}
+				{/* <YouTube videoId={yt.id} opts={opts} onReady={_onReady} /> */}
+				<ReactPlayer
+					url={yt.url}
+					playsinline={true}
+					playing={true}
+					height="0px"
+					width="0px"
+				/>
+				{/* // ) : null} */}
+			</form>
+
+			<div className="card-list">
+				{pics.map((pic) => (
+					<div className="card" key={pic.id}>
+						<img
+							className="card--image"
+							alt={pic.alt_description}
+							src={pic.urls.full}
+							width="50%"
+							height="50%"
+						></img>
+					</div>
+				))}{' '}
+			</div>
+		</>
+	);
+}
